d86b59f95f5f1e558be9d5834a86f7275e2167fd
Haider Nasir <haider.nasir6462@gmail.com>
2023-05-18 20:46:11 +0500

    add loader function and use useLoaderData hook

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,14 +3,13 @@ import {
   createRoutesFromElements,
   Route,
 } from "react-router-dom";
-import Root from "./components/root";
 import { NotFound } from "./pages/NotFound";
 import Contact from "./components/contact";
-
+import Root, { loader as rootLoader  } from "./components/root";
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<Root />} errorElement={<NotFound />}>
+      <Route path="/" element={<Root />} errorElement={<NotFound />} loader={rootLoader}>
         <Route path="/contacts/:id" element={<Contact />}></Route>
       </Route>
     </>
