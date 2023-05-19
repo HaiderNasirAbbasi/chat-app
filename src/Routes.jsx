836a0faf8d1cc881717d3328da45ffb977faecaa
Haider Nasir <haider.nasir6462@gmail.com>
2023-05-19 23:30:17 +0500

    add edit route and component

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,13 +4,33 @@ import {
   Route,
 } from "react-router-dom";
 import { NotFound } from "./pages/NotFound";
-import Contact from "./components/contact";
-import Root, { loader as rootLoader, action as rootAction  } from "./components/root";
+import Contact, { loader as contactLoader } from "./components/contact.jsx";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./components/root";
+import Edit, { action as editAction } from "./components/Edit";
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<Root />} errorElement={<NotFound />} loader={rootLoader} action={rootAction}>
-        <Route path="/contacts/:id" element={<Contact />}></Route>
+      <Route
+        path="/"
+        element={<Root />}
+        errorElement={<NotFound />}
+        loader={rootLoader}
+        action={rootAction}
+      >
+        <Route
+          path="/contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+        ></Route>
+        <Route
+          path="/contacts/:contactId/edit"
+          element={<Edit />}
+          loader={contactLoader}
+          action={editAction}
+        ></Route>
       </Route>
     </>
   )
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,15 +2,14 @@ import { Link, useRouteError } from "react-router-dom";
 
 export const NotFound = () => {
   const error = useRouteError();
+  console.log(error);
 
   return (
     <main className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
       <h1 className="text-9xl font-extrabold text-white tracking-widest">
         {error.status}
       </h1>
-      <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute text-white">
-        <i>{error.statusText || error.message}</i>
-      </div>
+
       <button className="mt-5">
         <a className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
           <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
@@ -20,6 +19,9 @@ export const NotFound = () => {
           </span>
         </a>
       </button>
+      <div className="bg-[#cf2d2d] px-2 text-sm rounded  text-white">
+        <i>{error.statusText || error.message}</i>
+      </div>
     </main>
   );
 };
